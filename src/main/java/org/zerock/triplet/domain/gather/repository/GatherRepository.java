3d10c19849fbfdb07308032e913946828c5cb8b7
Joo Won <110383412+chqkq@users.noreply.github.com>
2025-10-11 00:25:29 +0900

    Feat: 배포용 파일

--- src/main/java/org/zerock/triplet/domain/gather/repository/GatherRepository.java
+++ src/main/java/org/zerock/triplet/domain/gather/repository/GatherRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.triplet.domain.gather.entity.Gather;
 
 public interface GatherRepository extends JpaRepository<Gather, Long> {
+    Gather findGatherById(Long gatherId);
 }
